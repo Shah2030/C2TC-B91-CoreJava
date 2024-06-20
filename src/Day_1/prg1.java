650ae689b73e244bb1b295e2db62f01e5d2454fb
abhij <abhijitshah.2030@gmail.com>
2024-06-20 11:42:31 +0530

    second

---
+++ src/Day_1/prg1.java
@@ -0,0 +1,16 @@
+package Day_1; //package name
+
+public class prg1 { //class name
+	
+	 //Instance of class (member functions and members)
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Printing a message in console with new line");
+		System.out.print("Printing a message in console without new line");
+		System.out.printf("Printing a message in console i.e., formated printing");
+		//Objects or variables
+		
+	}
+
+}
