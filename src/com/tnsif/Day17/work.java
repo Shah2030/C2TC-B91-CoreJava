0938890e3540ee665dc4a874968ca815cabb6a3b
abhij <abhijitshah.2030@gmail.com>
2024-07-25 17:45:35 +0530

    String opt Assignment

--- src/com/tnsif/Day17/work.java
+++ src/com/tnsif/Day17/work.java
@@ -1,11 +1,30 @@
 package com.tnsif.Day17;
+import java.util.Scanner;
+
+//UpperCase letters at last and rest letters in the same order Assignment
 
 public class work
 {
 	
 	public static void main(String[] args) 
 	{
-	
+		Scanner s=new Scanner(System.in);
+		System.out.print("Enter the string: ");
+		String sb=new String(s.nextLine());
+		String osc="";
+		String oss="";
+		for(int i=0;i<sb.length();i++)
+		{
+			String c=sb.charAt(i)+"";
+			if(c==" ")
+				oss+=" ";
+			else if(c==c.toLowerCase())
+				oss+=c; 
+			else
+				osc+=c;	
+		}
+		System.out.println(oss+osc);
+		s.close();
 	}
 
 }
